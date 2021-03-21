4fb67509559abff7144f9a99bd3f357188ffaf2a
Dmitry Nor <thepineoak@gmail.com>
2021-03-21 22:43:02 +0500

    misc

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist build (mkdir build)
 pushd build
-cl /nologo /Zi /EHsc /I"..\include" "..\src\*.cpp" 
+cl /nologo /Zi /EHsc /I"..\include" "..\src\pasc.cpp" "..\src\io.cpp" "..\src\ctoken.cpp" "..\src\clexer.cpp"
 popd
