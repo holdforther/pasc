42d31559cebc66c6c9fc54f9075b207bd3ce2de8
Dmitry Nor <thepineoak@gmail.com>
2021-04-18 17:10:50 +0500

    Error handling module

--- include/cerror.hpp
+++ include/cerror.hpp
@@ -1,24 +1,50 @@
 #pragma once
-#include <string>
 #include <vector>
+
+#include "ctoken.hpp"
 
 namespace pasc
 {
-    struct caret_position
-    {
-        unsigned nline;
-        unsigned ncol;  
-    };
-    
-    enum error_type
-    {
-        no_errors
+    enum error_code {
+        eec_no,
+        // Lexical errors
+        eec_too_many_symbols,
+        // Syntactic errors
+        eec_exp_program,
+        eec_exp_begin,
+        eec_exp_end,
+        eec_exp_identifier,
+        eec_exp_period,
+        eec_exp_comma,
+        eec_exp_colon,
+        eec_exp_semicolon,
+        eec_exp_double_period,
+        eec_exp_assignment,
+        eec_exp_left_parenthesis,
+        eec_exp_right_parenthesis,
+        eec_exp_then,
+        eec_exp_do,
+        eec_exp_equal,
+        eec_exp_record,
+        eec_exp_until,
+        eec_exp_multiplier,
+        eec_exp_direction,
+        eec_exp_const,
+        eec_invalid_operator,
+        eec_invalid_type,
+        eec_unknown
     };
 
-    struct ErrorList
+    struct CError
     {
-        std::vector<int> codes;
-        std::vector<size_t> rows;
-        std::vector<size_t> cols;
+        size_t row;
+        size_t col;
+        error_code code;
+        CError() : row(0), col(0), code(eec_no) {}
+        CError(size_t row, size_t col) : row(row), col(col), code(eec_no) {} 
+        CError(size_t row, size_t col, error_code code) : row(row), col(col), code(code) {}
+        std::string to_string() const;
     };
+    
+    typedef std::vector<CError> ErrorList;
 }
--- src/cerror.cpp
+++ src/cerror.cpp
@@ -1,6 +1,32 @@
 #include "cerror.hpp"
+#include <unordered_map>
 
-pasc::CError::CError(const std::string &err_msg)
-{
-    msg = err_msg;
+namespace pasc {
+    std::unordered_map<error_code, std::string> code_repr {
+        {eec_too_many_symbols, "too many symbols for a variable"},
+        {eec_exp_program, "expected \'PROGRAM\'"},
+        {eec_exp_begin, "expected \'BEGIN\'"},
+        {eec_exp_end, "expected \'END\'"},
+        {eec_exp_identifier, "expected identifier"},
+        {eec_exp_period, "expected \'.\'"},
+        {eec_exp_comma, "expected \',\'"},
+        {eec_exp_colon, "expected \':\'"},
+        {eec_exp_semicolon, "expected \';\'"},
+        {eec_exp_double_period, "expected \'..\'"},
+        {eec_exp_assignment, "expected \':=\'"},
+        {eec_exp_left_parenthesis, "expected \'(\'"},
+        {eec_exp_right_parenthesis, "expected \')\'"},
+        {eec_exp_then, "expected \'THEN\'"},
+        {eec_exp_do, "expected \'DO\'"},
+        {eec_exp_equal, "expected \'EQUAL\'"},
+        {eec_exp_record, "expected \'RECORD\'"},
+        {eec_exp_until, "expected \'UNTIL\'"},
+        {eec_exp_multiplier, "expected constant, identifier or \'(\'"},
+        {eec_invalid_operator, "invalid operator"},
+        {eec_unknown, "unknown error"}
+    };
+
+    std::string CError::to_string() const{
+        return code_repr.at(code);
+    }
 }
