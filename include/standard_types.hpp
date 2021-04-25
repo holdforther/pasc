e8e950ee9ab8e5ccb4467badc6d68e5e8e5c7e2d
Dmitry Nor <thepineoak@gmail.com>
2021-04-25 23:53:53 +0500

    List of Pascal standard types

---
+++ include/standard_types.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace pasc {
+    enum standard_type {
+        est_unknown,
+        est_integer,
+        est_char,
+        est_real,
+        est_boolean,
+        est_enum,
+        est_procedure,
+        est_function,
+        est_array,
+        est_record
+    };
+}
